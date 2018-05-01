f793727934d2563b2b9792556e90efb463cbb7ec
Nengz <nengzix@gmail.com>
2018-05-01 21:54:14 +0700

    add test

--- GW_NODE_AP/TrainController.py
+++ GW_NODE_AP/TrainController.py
@@ -107,3 +107,5 @@ def test():
     print(time.time() - tx)
     pwm.stop()
     GPIO.cleanup()
+
+test()
--- GW_NODE_AP/initialsystem.py
+++ GW_NODE_AP/initialsystem.py
@@ -2,17 +2,18 @@ from TrainController import controller,trainForward,trainStop
 import time
 
 def init_system():
+
     trainStop()
     trainForward()
     controller(20,0,0,0)
-    trainStop()
-    time.sleep(5)
-    trainForward()
-    controller(20,0,0,0)
-    trainStop()
+#    trainStop()
+#    time.sleep(5)
+#    trainForward()
+#    controller(20,0,0,0)
+#    trainStop()
 
 
 
 
 if __name__ == "__main__":
-    controller()
+    init_system()
