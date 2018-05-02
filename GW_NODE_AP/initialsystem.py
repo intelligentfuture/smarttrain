1796afd4cc4c7407d0a70c23f60f37f7ab35557d
CHATCHAWAL SANGKEETTRAKARN <conan@conan.in.th>
2018-05-02 16:43:40 +0700

    update init system

--- GW_NODE_AP/initialsystem.py
+++ GW_NODE_AP/initialsystem.py
@@ -3,9 +3,9 @@ import time
 
 def init_system():
 
-    call_train(7.5,40,0,0,0)
+    call_train(10,60,0,0,0)
     time.sleep(5)
-    call_train(7.5,40,0,0,0)
+    call_train(10,60,0,0,0)
 
 
 if __name__ == "__main__":
