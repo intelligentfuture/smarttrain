1903e37d438b54d4ff0139ce6133be4d0075a2f6
CHATCHAWAL SANGKEETTRAKARN <conan@conan.in.th>
2018-05-02 15:57:40 +0700

    update init system

--- GW_NODE_AP/initialsystem.py
+++ GW_NODE_AP/initialsystem.py
@@ -5,7 +5,7 @@ def init_system():
 
     call_train(25,0,0,0)
     time.sleep(1)
-     call_train(25,0,0,0)
+    call_train(25,0,0,0)
     # time.sleep(1)
     # call_train(20,0,0,0)
     # time.sleep(1)
