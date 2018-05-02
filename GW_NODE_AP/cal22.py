56e1b5fc6822ca1853a514986ed374aa1e791791
CHATCHAWAL SANGKEETTRAKARN <conan@conan.in.th>
2018-05-02 21:44:34 +0700

    send speed to server

--- GW_NODE_AP/cal22.py
+++ GW_NODE_AP/cal22.py
@@ -97,11 +97,26 @@ def define_order(line):
         print(sensors_text)
 #        controller(Tref,x0,v0,t0)
 # tref=time for loop, x0 = current position, v0 = current speed, t0 = current time
-#        send_speed(uid,speed)
+        send_speed(uid,speed)
         if sum_dist>LENRM:
             sum_dist = 0
             # print(data_type,uid,speed,end='\r')
         sensors_text = ""
 
+    except Exception as e:
+        print(e)
+
+
+def send_speed(point,speed):
+    resp = dict()
+
+    print(point,speed)
+    url = 'http://200ok.in.th:7654/api/set-current-speed?point=%s&speed=%.2f'%(point,speed)
+    request = Request(url)
+    resp['status'] = 'ERROR'
+    try:
+        with urlopen(request) as response:
+            repy = json.loads(response.read().decode('utf-8'))
+            resp['status'] = 'OK'
     except Exception as e:
         print(e)
