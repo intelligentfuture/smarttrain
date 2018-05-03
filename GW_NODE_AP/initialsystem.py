b16e76871640a2e71dea579f7da758875217d282
CHATCHAWAL SANGKEETTRAKARN <conan@conan.in.th>
2018-05-03 19:56:20 +0700

    show dist(a,b)

--- GW_NODE_AP/initialsystem.py
+++ GW_NODE_AP/initialsystem.py
@@ -3,9 +3,9 @@ import time
 
 def init_system():
 
-    call_train(10,60,0,0,0)
+    call_train(10,40,0,0,0)
     time.sleep(5)
-    call_train(10,60,0,0,0)
+    call_train(10,40,0,0,0)
 
 
 if __name__ == "__main__":
